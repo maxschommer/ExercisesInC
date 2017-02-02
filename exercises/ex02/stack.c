15dbe19b03be84f15f25f0e8b458aaedc7f0ee84
maxschommer <maxschommer@gmail.com>
2017-02-01 23:34:58 -0500

    Finished the exercise

--- exercises/ex02/stack.c
+++ exercises/ex02/stack.c
@@ -8,25 +8,29 @@ License: GNU GPLv3
 #include <stdio.h>
 #include <stdlib.h>
 
-#define SIZE 5
+#define SIZE 5 /*This is defined in constant memory*/
 
-int *foo() {
+int *foo() {  /*Returns an array/pointer */
     int i;
-    int array[SIZE];
+    int array[5];
 
-    //printf("%p\n", array);
+    printf("%p\n", array);
 
-    for (i=0; i<SIZE; i++) {
-	array[i] = 42;
+    for (i=0; i<5; i++) {
+	array[i] = 42; /*Sets every value of the array to 42*/
     }
     return array;
 }
 
-void bar() {
+void bar() { /*This returns nothing, but modifies existing 
+    variables*/
     int i;
     int array[SIZE];
-
-    //printf("%p\n", array);
+    printf("%p\n", array); //This points to the same
+                           //space as the other array
+                           //because it has its own 
+                           //variable, and the other
+                           //ones were destroyed.
 
     for (i=0; i<SIZE; i++) {
 	array[i] = i;
@@ -34,9 +38,14 @@ void bar() {
 }
 
 int main()
+/*This is trying to print out "42" 5 times*/
 {
     int i;
-    int *array = foo();
+    int *array = foo(); /*Since foo returns a pointer, then 
+    array is assigned to a pointer, so it references array inside
+    the function.
+    Array is destroyed after the function is finished because
+    it is stack memory, and we get the error.*/
     bar();
 
     for (i=0; i<SIZE; i++) {
